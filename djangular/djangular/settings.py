38d960eae2d88a31b38ceb617aedf91210965fa2
vanros <vanessarodrigues.vj@gmail.com>
2018-06-25 09:58:28 -0300

    set db

--- djangular/djangular/settings.py
+++ djangular/djangular/settings.py
@@ -76,25 +76,17 @@ WSGI_APPLICATION = 'djangular.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
-    DATABASES = {
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbdjangular',
+        # 'NAME': os.path.join(BASE_DIR, 'mydb'),
+        'USER': 'dbmaster',
+        'PASSWORD': '0.senha.0',
+        'HOST': 'dbdjangular.cguzmlvh98pe.us-east-1.rds.amazonaws.com',
+        'PORT': '5432', # 8000 is default
     }
 }
-
 
 
 # Password validation
